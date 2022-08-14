9c40767cf0a19630a2fc012a53cb51427f9c8627
snehadongare0110 <98748503+snehadongare0110@users.noreply.github.com>
2022-08-14 22:48:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const api = "https://run.mocky.io/v3/9b184f9d-bf48-4467-9d8f-137ea0eba817";
+
+async function getapi(url) {
+    const response = await fetch(url);
+
+    const emp = await response.json();
+    console.log(emp);
+
+    if (response) {
+        hideloader();
+
+    }
+    show(emp);
+}
+
+getapi(api);
+
+function hideloader() {
+    document.getElementById("loading").style.display = "none";
+}
+
+function show(emp) {
+    let message = emp.message;
+ 
+
+ 
+    let table =
+        `<tr>
+					<th>name</th>
+					<th>office</th>
+					<th>position</th>
+					<th>salary</th>
+					</tr>`;
+
+
+
+    for (let i = 0; i < emp.data.length; i++) {
+    let obj = emp.data[i];
+    console.log(obj);
+   
+        table += 
+        `<tr>
+                        <td>${obj.name} </td>
+                        <td>${obj.office}</td>
+                        <td>${obj.position}</td>
+                        <td>${obj.salary}</td>		
+                    </tr>`;
+
+    }
+
+    document.getElementById("table").innerHTML = table;
+    document.getElementById("message").innerHTML = message;
+}
